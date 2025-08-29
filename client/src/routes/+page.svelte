6d7a45c8d1f64f5a77a188ebc22df16bdd8b75fb
Luka Hietala <luka.hietala08@gmail.com>
2025-08-29 21:26:17 +0300

    base css for game area

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,1 +1,45 @@
-<h1>:D</h1>
+<main class="h-screen relative overflow-hidden">
+   <!-- enemy left -->
+    <div class="absolute top-4 left-4">
+        <div class="grid grid-cols-5 gap-1">
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+        </div>
+    </div>
+
+    <!-- enemy center -->
+    <div class="absolute top-4 left-1/2 transform -translate-x-1/2">
+        <div class="grid grid-cols-5 gap-1">
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+        </div>
+    </div>
+
+    <!-- enemy right -->
+    <div class="absolute top-4 right-4">
+        <div class="grid grid-cols-5 gap-1">
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+        </div>
+    </div>
+
+    <!-- own -->
+    <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+        <div class="grid grid-cols-5 gap-1">
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+            <div class="w-14 h-24 bg-gray-500"></div>
+        </div>
+    </div>
+</main>
